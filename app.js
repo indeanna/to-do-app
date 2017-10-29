25d3c6487ca345f2f91693d784a32534d0d8d429
DeAnne McCammon <indeannajone44@gmail.com>
2017-10-29 18:37:37 -0400

    Added delete button and functionality

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+  const delButton = document.getElementById('delButton');
+
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -13,26 +15,34 @@ function onReady() {
 
     //create new input
     let checkbox = document.createElement('input');
+    let delButton = document.createElement("button");
 
     // set the inputs type to checkbox
     checkbox.type = "checkbox";
+    delButton.type = "button";
 
     // set the title
     newLi.textContent = title
+    delButton.textContent = "Delete To-do"
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+    newLi.appendChild(delButton);
 
     //attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
     newToDoText.value = '';
+
+  delButton.addEventListener("click", event => {
+    event.preventDefault();
+    newLi.parentNode.removeChild(newLi);
+  })
   });
 
 }
 
 window.onload = function() {
-  OnReady()_
-  
+  onReady()
 };
